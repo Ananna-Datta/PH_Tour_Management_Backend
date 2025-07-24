73bf86322f1034e3d1654b6caf193cdc23ca3e0c
Ananna-Datta <anannadatta45@gmail.com>
2025-07-24 12:48:53 +0600

    user section

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
-import  express, { Request, Response }  from 'express';
 
+import  express, {  Request, Response }  from 'express';
+import cors from "cors"
+import { router } from './app/routes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1",router)
 
 app.get("/", (req:Request,res:Response)=>{
     res.status(200).json({
         success:true,
-        massage:"Welcome to Tour management system backend"
+        message:"Welcome to Tour management system backend"
     })
 })
+app.use(notFound)
+app.use(globalErrorHandler)
 
 export default app;
