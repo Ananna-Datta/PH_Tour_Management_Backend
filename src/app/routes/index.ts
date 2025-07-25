49d34f351c655a385d94e9436cd13fffd083942a
Ananna-Datta <anannadatta45@gmail.com>
2025-07-25 09:51:02 +0600

    Part 2

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserRoutes } from "../modulers/user/user.route";
+import { AuthRoutes } from "../modulers/auth/auth.route";
 // import { UserRoutes } from "../modulers/user/user.route";
 
 export const router = Router();
@@ -8,6 +9,9 @@ const moduleRoutes = [
     {
         path:"/user",
         route:UserRoutes
+    },{
+        path: "/auth",
+        route: AuthRoutes
     }
 ]
 
